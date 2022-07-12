965a7acf47b232e321439ea8407596ac733bb70f
Sweta Swarnakar <swarnakarsweta@gmail.com>
2022-07-12 15:03:04 +0530

    added variables

--- dist/assets/fresh-team.js
+++ dist/assets/fresh-team.js
@@ -1242,7 +1242,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("fresh-team/app")["default"].create({"name":"fresh-team","version":"0.0.0+0ad5986c"});
+            require("fresh-team/app")["default"].create({"name":"fresh-team","version":"0.0.0+797d4446"});
           }
         
 //# sourceMappingURL=fresh-team.map
