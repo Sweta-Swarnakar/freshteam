3ba35dd6abf4d4c8d0fc7fd2aa25c5d19ac6343c
Sweta Swarnakar <swarnakarsweta@gmail.com>
2022-07-12 15:46:26 +0530

    added padding

--- dist/assets/fresh-team.js
+++ dist/assets/fresh-team.js
@@ -1183,7 +1183,7 @@
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-  exports.default = Ember.HTMLBars.template({ "id": "Rp/CO7/w", "block": "{\"symbols\":[],\"statements\":[[7,\"body\"],[11,\"class\",\"main\"],[9],[0,\"\\n\\n  \"],[7,\"div\"],[11,\"class\",\"left-nav\"],[9],[0,\"\\n    \"],[7,\"div\"],[11,\"class\",\"icon-logo-container\"],[9],[0,\"\\n      \"],[1,[27,\"svg-jar\",[\"ft\"],[[\"class\"],[\"icon-logo\"]]],false],[0,\"\\n    \"],[10],[0,\"\\n  \"],[10],[0,\"\\n\\n  \"],[7,\"header\"],[11,\"class\",\"header\"],[9],[0,\"\\n    \"],[7,\"img\"],[11,\"class\",\"logo\"],[11,\"src\",\"assets/brand-header-logo.png\"],[11,\"alt\",\"freshteam logo\"],[9],[10],[0,\"\\n    \"],[7,\"div\"],[11,\"class\",\"header-title\"],[9],[0,\"Talent Center\"],[10],[0,\"\\n  \"],[10],[0,\"\\n  \\n  \"],[7,\"main\"],[11,\"class\",\"content\"],[9],[0,\"\\n\\n  \"],[10],[0,\"\\n\\n  \"],[7,\"footer\"],[11,\"class\",\"footer\"],[9],[0,\"\\n    \"],[7,\"span\"],[11,\"class\",\"footer-content\"],[9],[0,\"\\n      HR Software by Freshworks | Knowledge Base\\n    \"],[10],[0,\"\\n  \"],[10],[0,\"\\n\"],[10],[0,\"\\n\"]],\"hasEval\":false}", "meta": { "moduleName": "fresh-team/templates/application.hbs" } });
+  exports.default = Ember.HTMLBars.template({ "id": "oWmO8nUo", "block": "{\"symbols\":[],\"statements\":[[7,\"body\"],[11,\"class\",\"main\"],[9],[0,\"\\n\\n  \"],[7,\"div\"],[11,\"class\",\"left-nav\"],[9],[0,\"\\n    \"],[7,\"div\"],[11,\"class\",\"icon-logo-container\"],[9],[0,\"\\n      \"],[1,[27,\"svg-jar\",[\"ft\"],[[\"class\"],[\"icon-logo\"]]],false],[0,\"\\n    \"],[10],[0,\"\\n  \"],[10],[0,\"\\n\\n  \"],[7,\"header\"],[11,\"class\",\"header\"],[9],[0,\"\\n    \"],[7,\"img\"],[11,\"class\",\"logo\"],[11,\"src\",\"assets/brand-header-logo.png\"],[11,\"alt\",\"freshteam logo\"],[9],[10],[0,\"\\n    \"],[7,\"span\"],[11,\"class\",\"header-title\"],[9],[0,\"Talent Center\"],[10],[0,\"\\n  \"],[10],[0,\"\\n  \\n  \"],[7,\"main\"],[11,\"class\",\"content\"],[9],[0,\"\\n\\n  \"],[10],[0,\"\\n\\n  \"],[7,\"footer\"],[11,\"class\",\"footer\"],[9],[0,\"\\n    \"],[7,\"p\"],[11,\"class\",\"footer-content\"],[9],[0,\"\\n      HR Software by Freshworks | Knowledge Base\\n    \"],[10],[0,\"\\n  \"],[10],[0,\"\\n\"],[10],[0,\"\\n\"]],\"hasEval\":false}", "meta": { "moduleName": "fresh-team/templates/application.hbs" } });
 });
 ;define('fresh-team/templates/components/ember-popper-targeting-parent', ['exports', 'ember-popper/templates/components/ember-popper-targeting-parent'], function (exports, _emberPopperTargetingParent) {
   'use strict';
@@ -1242,7 +1242,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("fresh-team/app")["default"].create({"name":"fresh-team","version":"0.0.0+965a7acf"});
+            require("fresh-team/app")["default"].create({"name":"fresh-team","version":"0.0.0+45d12f45"});
           }
         
 //# sourceMappingURL=fresh-team.map
