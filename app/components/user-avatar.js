e1c25dfa225f5cdf68059b792fdc21d1ab2ffac0
Sweta Swarnakar <swarnakarsweta@gmail.com>
2022-07-14 19:16:11 +0530

    completed part 2

--- app/components/user-avatar.js
+++ app/components/user-avatar.js
@@ -1,22 +1,36 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+/**
+ * 1. Go through docs very well.
+ * 2. Component lifecycle.
+ * 3. How HBS and JS can work together.
+ */
+
+//'#' + Math.random().toString(16).substr(-6);  random colors
+
+// classNames
+// classNameBindings
 
 
 export default Component.extend({
 
-   
-    init() {
-        this._super(...arguments);
-        this.set('errors', []);
+  // classNames: ['circle-div'],
+
+  randomColor: computed(function(){
       
+    var colors = ['red', 'green', 'blue', 'orange', 'yellow', 'purple', 'violet', 'pink', 'coral'];
 
-      
-  var data = Object.keys(this.attrs).map(k => this[k]) 
+    return `${colors[Math.floor(Math.random() * colors.length)]}`
 
-
-    let temp = data[0];
-    let firstChar = temp[0];
-    console.log(firstChar);
-    }
+  }),
+  
+ firstChar: computed('user', function()
+ {
+    let users = this.user;
+     return users.first_name[0];
+ })
+  
 
 });
 
--- tests/integration/components/user-avatar-test.js
+++ tests/integration/components/user-avatar-test.js
@@ -2,25 +2,55 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
+import { set } from '@ember/object';
 
-module('Integration | Component | user-avatar', function(hooks) {
+module('Integration | Component | user-avatar', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  // test('it renders img when a user with avatar is passed', async function(assert) {
+  //   let store = ;
+  //   this.user = this.store.createRecord('user', {
+  //     firstName: '',
+  //     avatar: ''
+  //   })
 
-    await render(hbs`{{user-avatar}}`);
+  //   await render(hbs`{{user-avatar user=user}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
 
-    // Template block usage:
-    await render(hbs`
-      {{#user-avatar}}
-        template block text
-      {{/user-avatar}}
-    `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  //});
+
+
+  hooks.beforeEach(function () {
+    this.store = this.owner.lookup('service:store');
+    this.user = this.store.createRecord('user', {
+
+      img_url: 'fake.png',
+      first_name: 'test-first_name',
+      last_name: 'test-last_name',
+      email: 'test-email',
+      team: 'test-team',
+      joiningDate: 'test-date'
+
+    });
   });
+
+//   test('it renders img when a user with avatar is passed', async function(assert) {
+//     await render(hbs`{{user-avatar user = this.user}}`);
+//     //assert.equal(this.element.find('.avatar img').innerHtml.includes('img'));
+
+  
+//     assert.dom('[data-test-id="user-avatar"]').exists('The img tag exists');
+//     //assert.dom('[data-test-id="user-avatar"]').doesNotexist('The img tag does not exist');
+
+//  });
+ test('it renders img when a user with avatar is passed', async function(assert) {
+  set(this, 'user.img_url', null); 
+  await render(hbs`{{user-avatar user = this.user}}`);
+  
+  assert.dom('[data-test-id="user-avatar"]').doesNotExist('The img tag does not exist');
+  assert.dom('[data-test-id="no-user-avatar"]').exists();
+
+});
 });
