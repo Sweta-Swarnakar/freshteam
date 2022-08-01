be973291c53a674ed3042177d2171dba6df01111
Sweta Swarnakar <swarnakarsweta@gmail.com>
2022-08-01 11:06:35 +0530

    added

--- app/components/user-form.js
+++ app/components/user-form.js
@@ -16,7 +16,7 @@ export default Component.extend({
       set(this, 'userData.team', team);
     },
 
-    setAvatar(avatar) {
+    setAvatar() {
 
       var input = document.querySelector(".upload").files[0];
       var path = (window.URL || window.webkitURL).createObjectURL(input);
