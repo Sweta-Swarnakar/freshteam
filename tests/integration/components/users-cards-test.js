6de0aabdd7e1f07cf2e0b7a8ffdfb2ade4fb08e7
Sweta Swarnakar <swarnakarsweta@gmail.com>
2022-08-09 01:14:47 +0530

    added style

--- tests/integration/components/users-cards-test.js
+++ tests/integration/components/users-cards-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, click,  } from '@ember/test-helpers';
-// typeIn, waitUntil
+import { render, click,typeIn} from '@ember/test-helpers';
+
 import hbs from 'htmlbars-inline-precompile';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
@@ -10,68 +10,68 @@ module('Integration | Component | users-cards', function (hooks) {
   setupRenderingTest(hooks);
   setupMirage(hooks);
 
-  //   test("I can view the users", async function (assert) {
+    test("I can view the users", async function (assert) {
 
-  //     this.users = this.server.createList('user', 15);
-  //     await render(hbs`{{users-cards model=this.users}}`);
+      this.users = this.server.createList('user', 15);
+      await render(hbs`{{users-cards model=this.users}}`);
 
-  //     assert.dom(".card").exists({ count: 15 }, 'shows 15 data')
-  //   });
+      assert.dom(".card").exists({ count: 15 }, 'shows 15 data')
+    });
 
-  //   test("I can view details of card ", async function (assert) {
+    test("I can view details of card ", async function (assert) {
 
-  //     this.users = this.server.createList('user', 15);
-  //     await render(hbs`{{users-cards model=this.users}}`);
+      this.users = this.server.createList('user', 15);
+      await render(hbs`{{users-cards model=this.users}}`);
 
-  //     assert.dom('.circle-div').exists('the circle is present');
-  //     let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
-  //     assert.dom('.name').hasTextContaining(name);
-  //     assert.dom('.email').hasText(this.users.firstObject.email);
-  //   });
+      assert.dom('.circle-div').exists('the circle is present');
+      let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
+      assert.dom('.name').hasTextContaining(name);
+      assert.dom('.email').hasText(this.users.firstObject.email);
+    });
 
-  //   test("I can view filtered cards", async function (assert) {
+    test("I can view filtered cards", async function (assert) {
 
-  //     this.users = this.server.createList('user', 15);
-  //     await render(hbs`{{users-cards model=this.users}}`);
+      this.users = this.server.createList('user', 15);
+      await render(hbs`{{users-cards model=this.users}}`);
 
-  //     await click(".dropdown-toggle");
+      await click(".dropdown-toggle");
 
-  //     await click('[data-test-id="Freshteam"]');
+      await click('[data-test-id="Freshteam"]');
 
-  //     let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
-  //     assert.dom('.name').hasTextContaining(name);
-  //     assert.dom('.email').hasText(this.users.firstObject.email);
-  //  });
+      let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
+      assert.dom('.name').hasTextContaining(name);
+      assert.dom('.email').hasText(this.users.firstObject.email);
+   });
 
-  //  test("I can view sorted cards", async function (assert) {
+   test("I can view sorted cards", async function (assert) {
 
-  //   this.users = this.server.createList('user', 15);
-  //   await render(hbs`{{users-cards model=this.users}}`);
+    this.users = this.server.createList('user', 15);
+    await render(hbs`{{users-cards model=this.users}}`);
 
-  //   await click('[data-test-id="sort-click"]');
+    await click('[data-test-id="sort-click"]');
 
-  //   await click('[data-test-id="first_name"]');
+    await click('[data-test-id="first_name"]');
 
-  //   let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
-  //   assert.dom('.name').hasTextContaining(name);
-  //   assert.dom('.email').hasText(this.users.firstObject.email);
-  // });
+    let name = [this.users.firstObject.first_name, this.users.firstObject.last_name].join(' ');
+    assert.dom('.name').hasTextContaining(name);
+    assert.dom('.email').hasText(this.users.firstObject.email);
+  });
 
 
-  // test("I can view searched cards", async function (assert) {
+  test("I can view searched cards", async function (assert) {
 
-  //   this.users = this.server.createList('user', 15);
+    this.users = this.server.createList('user', 15);
 
-  //   await render(hbs`{{users-cards model=this.users}}`);
+    await render(hbs`{{users-cards model=this.users}}`);
 
-  //   await typeIn('input', '14');
+    await typeIn('input', '14');
 
-  //   assert.dom(".card").exists({ count: 1 }, 'shows Person 1 card')
-  //   let name = [this.users.lastObject.first_name, this.users.lastObject.last_name].join(' '); 
-  //   assert.dom("strong.name").hasTextContaining(name);
-  //   assert.dom('.email').hasText(this.users.lastObject.email);
+    assert.dom(".card").exists({ count: 1 }, 'shows Person 1 card')
+    let name = [this.users.lastObject.first_name, this.users.lastObject.last_name].join(' '); 
+    assert.dom("strong.name").hasTextContaining(name);
+    assert.dom('.email').hasText(this.users.lastObject.email);
 
-  // });
+  });
 
   test("I can see the users after deleting", async function (assert) {
 
